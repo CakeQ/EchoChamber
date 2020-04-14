6526b495ce9490479aea9b6c076e9e7d44d23528
Padraic O'Donnell <PODonnell@Sumo-Digital.com>
2020-04-14 17:35:44 +0100

    Adding echo emitter

---
+++ Source/EchoChamber/Echoes/ECEchoEmitter.h
@@ -0,0 +1,18 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ECEchoEmitter.generated.h"
+
+UCLASS(minimalapi)
+class AECEchoEmitter : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AECEchoEmitter();
+};
+
+
+
